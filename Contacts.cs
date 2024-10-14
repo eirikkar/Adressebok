c934d529a9bdc9c81d99fc1686c083746517d385
eirikkar <eirikkar@proton.me>
2024-10-14 14:36:31 +0200

    Removed unused function, simplyfied using statements, added ReassignIds function

--- Contacts.cs
+++ Contacts.cs
@@ -25,102 +25,98 @@ public class Contacts
         return db;
     }
 
-    public static SqliteConnection CloseDatabase()
-    {
-        SqliteConnection sqliteCon;
-        sqliteCon = new SqliteConnection("Data Source=db.sqlite");
-        try
-        {
-            sqliteCon.Close();
-        }
-        catch (SqliteException e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        return sqliteCon;
-    }
-
     public static void CreateTable()
     {
-        using (SqliteConnection sqliteCon = InitDatabase())
-        {
-            sqliteCon.Open();
-            SqliteCommand sqliteCommand;
-            string createSQL =
-                "CREATE TABLE IF NOT EXISTS Contacts (Id INTEGER PRIMARY KEY, Name TEXT NOT NULL, PhoneNumber INTEGER NOT NULL, Email TEXT NOT NULL)";
-            sqliteCommand = sqliteCon.CreateCommand();
-            sqliteCommand.CommandText = createSQL;
-            sqliteCommand.ExecuteNonQuery();
-        }
+        using SqliteConnection sqliteCon = InitDatabase();
+        sqliteCon.Open();
+        SqliteCommand sqliteCommand;
+        string createSQL =
+            "CREATE TABLE IF NOT EXISTS Contacts (Id INTEGER PRIMARY KEY, Name TEXT NOT NULL, PhoneNumber INTEGER NOT NULL, Email TEXT NOT NULL)";
+        sqliteCommand = sqliteCon.CreateCommand();
+        sqliteCommand.CommandText = createSQL;
+        sqliteCommand.ExecuteNonQuery();
     }
 
     public static int AddContact(string name, int phoneNumber, string email)
     {
-        using (SqliteConnection sqliteCon = InitDatabase())
-        {
-            sqliteCon.Open();
-            using (var sqliteCommand = sqliteCon.CreateCommand())
-            {
-                sqliteCommand.CommandText =
-                    "INSERT INTO Contacts (Name, PhoneNumber, Email) VALUES (@Name, @PhoneNumber, @Email)";
-                sqliteCommand.Parameters.AddWithValue("@Name", name);
-                sqliteCommand.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
-                sqliteCommand.Parameters.AddWithValue("@Email", email);
-                sqliteCommand.ExecuteNonQuery();
+        using SqliteConnection sqliteCon = InitDatabase();
+        sqliteCon.Open();
+        using var sqliteCommand = sqliteCon.CreateCommand();
+        sqliteCommand.CommandText =
+            "INSERT INTO Contacts (Name, PhoneNumber, Email) VALUES (@Name, @PhoneNumber, @Email)";
+        sqliteCommand.Parameters.AddWithValue("@Name", name);
+        sqliteCommand.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+        sqliteCommand.Parameters.AddWithValue("@Email", email);
+        sqliteCommand.ExecuteNonQuery();
 
-                sqliteCommand.CommandText = "SELECT last_insert_rowid()";
-                int id = Convert.ToInt32(sqliteCommand.ExecuteScalar());
-                return id;
-            }
-        }
+        sqliteCommand.CommandText = "SELECT last_insert_rowid()";
+        int id = Convert.ToInt32(sqliteCommand.ExecuteScalar());
+        return id;
     }
 
     public static int EditContact(int id, string name, int phoneNumber, string email)
     {
-        using (SqliteConnection sqliteCon = InitDatabase())
-        {
-            sqliteCon.Open();
-            using (var sqliteCommand = sqliteCon.CreateCommand())
-            {
-                sqliteCommand.CommandText =
-                    "UPDATE Contacts SET Name = @Name, PhoneNumber = @PhoneNumber, Email = @Email WHERE Id = @Id";
-                sqliteCommand.Parameters.AddWithValue("@Id", id);
-                sqliteCommand.Parameters.AddWithValue("@Name", name);
-                sqliteCommand.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
-                sqliteCommand.Parameters.AddWithValue("@Email", email);
-                sqliteCommand.ExecuteNonQuery();
-                return id;
-            }
-        }
+        using SqliteConnection sqliteCon = InitDatabase();
+        sqliteCon.Open();
+        using var sqliteCommand = sqliteCon.CreateCommand();
+        sqliteCommand.CommandText =
+            "UPDATE Contacts SET Name = @Name, PhoneNumber = @PhoneNumber, Email = @Email WHERE Id = @Id";
+        sqliteCommand.Parameters.AddWithValue("@Id", id);
+        sqliteCommand.Parameters.AddWithValue("@Name", name);
+        sqliteCommand.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+        sqliteCommand.Parameters.AddWithValue("@Email", email);
+        sqliteCommand.ExecuteNonQuery();
+        return id;
     }
 
     public static int DeleteContact(int id)
     {
-        using (SqliteConnection sqliteCon = InitDatabase())
-        {
-            sqliteCon.Open();
+        using SqliteConnection sqliteCon = InitDatabase();
+        sqliteCon.Open();
 
-            using (var sqliteCommand = sqliteCon.CreateCommand())
-            {
-                sqliteCommand.CommandText = "DELETE FROM Contacts WHERE Id = @Id";
-                sqliteCommand.Parameters.AddWithValue("@Id", id);
-                sqliteCommand.ExecuteNonQuery();
-                return id;
-            }
+        using (var sqliteCommand = sqliteCon.CreateCommand())
+        {
+            sqliteCommand.CommandText = "DELETE FROM Contacts WHERE Id = @Id";
+            sqliteCommand.Parameters.AddWithValue("@Id", id);
+            sqliteCommand.ExecuteNonQuery();
         }
+        ReassignIds();
+        return id;
     }
 
     public static int GetCount()
     {
-        using (SqliteConnection sqliteCon = InitDatabase())
+        using SqliteConnection sqliteCon = InitDatabase();
+        sqliteCon.Open();
+        using var sqliteCommand = sqliteCon.CreateCommand();
+        sqliteCommand.CommandText = "SELECT COUNT(*) FROM Contacts";
+        int count = Convert.ToInt32(sqliteCommand.ExecuteScalar());
+        return count;
+    }
+
+    public static void ReassignIds()
+    {
+        using var db = InitDatabase();
+        db.Open();
+        using var transaction = db.BeginTransaction();
+        var cmd = new SqliteCommand("SELECT id FROM Contacts ORDER BY id", db, transaction);
+        var reader = cmd.ExecuteReader();
+        int newId = 1;
+
+        while (reader.Read())
         {
-            sqliteCon.Open();
-            using (var sqliteCommand = sqliteCon.CreateCommand())
-            {
-                sqliteCommand.CommandText = "SELECT COUNT(*) FROM Contacts";
-                int count = Convert.ToInt32(sqliteCommand.ExecuteScalar());
-                return count;
-            }
+            int oldId = reader.GetInt32(0);
+            var updateCmd = new SqliteCommand(
+                "UPDATE Contacts SET id = @newId WHERE id = @oldId",
+                db,
+                transaction
+            );
+            updateCmd.Parameters.AddWithValue("@newId", newId);
+            updateCmd.Parameters.AddWithValue("@oldId", oldId);
+            updateCmd.ExecuteNonQuery();
+            newId++;
         }
+
+        transaction.Commit();
     }
 }
